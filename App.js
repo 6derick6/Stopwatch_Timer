d3fd19d37b3ed68afcae8ea9aaf22328130e61b2
6derick6 <6derick6@gmail.com>
2023-04-03 13:02:31 -0300

    App timer 1/6

--- App.js
+++ App.js
@@ -1,11 +1,55 @@
 import { StatusBar } from 'expo-status-bar';
+import React, {useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Picker } from '@react-native-picker/picker';
 
 export default function App() {
+
+  console.disableYellowBox = true;
+  const [segundos,setarSegundos] = useState(0);
+  const [minutos,setarminutos] = useState(0);
+
+  const [alarmSound,setarAlarmSound] = useState([
+    {
+      selecionado: true,
+      som:'alarme 1',
+      file: 'alarme1.mp3'
+    },
+
+    {
+      selecionado: false,
+      som:'alarme 2',
+      file: 'alarme2.mp3'
+    },
+  ]);
+
+  var numeros = [];
+  for(var i = 1; i<=60; i++){
+    numeros.push(i);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={{color:'white',fontSize:30}}>Selecione o seu tempo:</Text>
+      <View style={{flexDirection:'row'}}>
+        <Picker style={{ height: 50, width: 100 }}>
+          {
+          numeros.map(function(val){
+            return(<Picker.Item label={val.toString()} value={val.toString()} />);
+          })
+          }
+        </Picker>
+
+        <Picker style={{ height: 50, width: 100 }}>
+          {
+          numeros.map(function(val){
+            return(<Picker.Item label={val.toString()} value={val.toString()} />);
+          })
+          }
+        </Picker>
+        <StatusBar style="auto" />
+      </View>
+
     </View>
   );
 }
@@ -13,7 +57,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'rgb(65,105,225)',
     alignItems: 'center',
     justifyContent: 'center',
   },
